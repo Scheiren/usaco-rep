843636a982ba8847d2936aa55f1ab43420f4cc7b
Nguyen Vu Hoang Anh <nguyenvuhoanganh1309@gmail.com>
2024-09-02 19:21:35 +0700

    Update badge.cpp

--- Introduction to Functional Graph/badge.cpp
+++ Introduction to Functional Graph/badge.cpp
@@ -1,52 +1,41 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 const long long INF = 1e9 + 10, mod = 1e9 + 7;
 const long long maxN = 2e5;
-
+ 
 class Solution
 {
 public:
-    int cycleNode = -1;
-    map<int, int> tortoiseAndHare2(vector<int> &base, int startingNode)
+    void tortoiseAndHare2(vector<int> &base, int startingNode)
     {
         int t, h;
-        map<int, int> mp;
         t = base[startingNode];
         h = base[base[startingNode]];
-
+ 
         while (t != h)
         {
             t = base[t];
             h = base[base[h]];
         }
-
+ 
         t = startingNode;
-
+ 
         while (t != h)
         {
             t = base[t];
             h = base[h];
         }
-
-        mp[t]++;
-        t = base[t];
-        mp[t]++;
-
-        while (t != h)
-        {
-            t = base[t];
-            mp[t]++;
-        }
-        return mp;
+ 
+        cout << t + 1 << " ";
     }
-
+ 
     void solve()
     {
         int n;
         cin >> n;
-
+ 
         vector<int> base(n, 0);
         for (int i = 0; i < n; i++)
         {
@@ -54,45 +43,24 @@ public:
             cin >> a;
             base[i] = --a;
         }
-
-        map<int, int> cycle;
-
-        cycle = tortoiseAndHare2(base, 0);
-
+ 
         for (int i = 0; i < n; i++)
         {
-            if (cycle[i])
-            {
-                cout << i + 1 << " ";
-                continue;
-            }
-
-            int t = i;
-
-            while (!cycle[t] && cycleNode == -1)
-            {
-                t = base[t];
-                if (cycle[t])
-                {
-                    cycleNode = t;
-                    break;
-                }
-            }
-            cout << t + 1 << " ";
+            tortoiseAndHare2(base, i);
         }
-
+ 
         return;
     }
-
+ 
 private:
 };
-
+ 
 signed main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
-
+ 
     Solution().solve();
-
+ 
     return 0;
 }
